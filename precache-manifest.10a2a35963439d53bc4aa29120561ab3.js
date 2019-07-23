f33ef16cb148d8be9f7027c17fdf6563fa932734
Kamil Seredziński <kamilsere@gmail.com>
2019-07-23 16:24:12 +0200

    Updates

---
+++ precache-manifest.10a2a35963439d53bc4aa29120561ab3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc38f5f22ba639c2357d1e96bec0e46",
+    "url": "/react-typing/index.html"
+  },
+  {
+    "revision": "b8641b6321e131e24fa9",
+    "url": "/react-typing/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-typing/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b8641b6321e131e24fa9",
+    "url": "/react-typing/static/js/main.a22341b4.chunk.js"
+  },
+  {
+    "revision": "8e2049d66212cd2f7c28",
+    "url": "/react-typing/static/js/runtime~main.cceb7fd6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nazwa/precache-manifest.aca31bd0a92c06def80cff61c2c08f69.js"
+  "/react-typing/precache-manifest.10a2a35963439d53bc4aa29120561ab3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nazwa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-typing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
