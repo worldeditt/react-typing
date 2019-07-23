153189b8df6eb631a4971a15860eb30d9aac6e37
Kamil Seredziński <kamilsere@gmail.com>
2019-07-23 16:19:35 +0200

    Updates

---
+++ precache-manifest.aca31bd0a92c06def80cff61c2c08f69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d5d65129b26a1e63c530936dae4d63",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "f3d81eebcd8283d16c6c",
+    "url": "/nazwa/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/nazwa/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f3d81eebcd8283d16c6c",
+    "url": "/nazwa/static/js/main.f99ca273.chunk.js"
+  },
+  {
+    "revision": "52e0c372864d619aa40f",
+    "url": "/nazwa/static/js/runtime~main.e23c9a83.js"
+  }
+]);
